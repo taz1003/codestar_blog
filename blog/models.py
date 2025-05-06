7fd87d8c7d249bc50439434209cb088b80a4c64c
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-06 17:16:05 +0200

    Update models

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,3 +17,27 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     exerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"{self.title} || written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+        
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
