957600c01a394116a33b1f860adae7dd810b89ad
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-07 22:00:36 +0200

    Fix views.py file

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post, get_object_or_404
+from .models import Post
 
 
 # Create your views here.
@@ -12,7 +12,7 @@ class PostList(generic.ListView):
 
 def post_detail(request, slug):
     """
-        Display an individual :model:`blog.Post`.
+    Display an individual :model:`blog.Post`.
 
     **Context**
 
