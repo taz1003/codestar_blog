a8ac4e3011d445a61c9821a95f64de25174dfa5a
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-08 18:39:00 +0200

    Correct typo

--- about/views.py
+++ about/views.py
@@ -16,7 +16,7 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
-    
+
     return render(
         request,
         "about/about.html",
