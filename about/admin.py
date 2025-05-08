a9419628faf2f49edb702a446009aa06425fa28f
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-08 18:43:19 +0200

    Add collaborate form functionality

--- about/admin.py
+++ about/admin.py
@@ -14,6 +14,7 @@ class AboutAdmin(SummernoteModelAdmin):
 #       own projects, then inherit from admin.ModelAdmin like
 #       we do below.
 
+
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
 
--- blog/models.py
+++ blog/models.py
@@ -17,10 +17,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
         return f"{self.title} || written by {self.author}"
 
@@ -35,9 +35,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
-        
+
     def __str__(self):
         return f"Comment: {self.body} by {self.author}"
