c66ae4603135ff7686b8e71b2287aeadf5856b72
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-08 18:32:50 +0200

    Add form for collab in about app

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
+
 
 # Create your views here.
 
@@ -13,9 +15,13 @@ def about_me(request):
         An instance of :model:`about.About`.
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
     
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
